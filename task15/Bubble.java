d97c0c0df5dd88328a447fdb67d627f70d2ad929
Pradeep Jaiswal <159229353+Pradeepjaiswal27@users.noreply.github.com>
2025-03-19 08:45:40 +0530

    Update Bubble.java

--- task15/Bubble.java
+++ task15/Bubble.java
@@ -1,1 +1,22 @@
+package Task15;
 
+public class bubble {
+    public static void main(String[] args) {
+        int[] arr = { 8, 6, 3, 9, 5, 1 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
