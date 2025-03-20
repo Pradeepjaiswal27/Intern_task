695ae4ad0da2b6bd99e6deb6f2de1e39b05cda02
Pradeep Jaiswal <159229353+Pradeepjaiswal27@users.noreply.github.com>
2025-03-20 15:51:23 +0530

    Create Calculate.java

---
+++ Task18/Calculate.java
@@ -0,0 +1,25 @@
+package Task18;
+
+class Rectangle implements Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+public class Calculate {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+    }
+}
