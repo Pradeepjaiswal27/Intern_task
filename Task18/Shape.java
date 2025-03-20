c4cbe43183e659dab9ea8e272beab3a78171a30d
Pradeep Jaiswal <159229353+Pradeepjaiswal27@users.noreply.github.com>
2025-03-20 15:53:48 +0530

    Create Shape.java

---
+++ Task18/Shape.java
@@ -0,0 +1,18 @@
+package Task18;
+
+public interface Shape {
+    double calculateArea();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
