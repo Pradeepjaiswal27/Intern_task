13b3850ab504cc7d8ebd0a34df2cca89f4b8e9cd
Pradeep Jaiswal <159229353+Pradeepjaiswal27@users.noreply.github.com>
2025-03-21 17:28:51 +0530

    Create StudentObj.java

---
+++ Task22/StudentObj.java
@@ -0,0 +1,25 @@
+package Task22;
+
+class StudentObj {
+    private int id;
+    private String name;
+    private int age;
+    private String grade;
+
+    public StudentObj(int id, String name, int age, String grade) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
